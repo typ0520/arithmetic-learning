494f0070730364b3baa22f307eac52cebcf8f4fe
typ0520 <php12345@163.com>
2021-02-20 10:24:25 +0800

    ba-zi-fu-chuan-zhuan-huan-cheng-zheng-shu-lcof

--- src/com/example/ba_zi_fu_chuan_zhuan_huan_cheng_zheng_shu_lcof.java
+++ src/com/example/ba_zi_fu_chuan_zhuan_huan_cheng_zheng_shu_lcof.java
@@ -1,8 +1,58 @@
 package com.example;
 
+/**
+ 写一个函数 StrToInt，实现把字符串转换成整数这个功能。不能使用 atoi 或者其他类似的库函数。
+
+  
+
+ 首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。
+
+ 当我们寻找到的第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字组合起来，作为该整数的正负号；假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
+
+ 该字符串除了有效的整数部分之后也可能会存在多余的字符，这些字符可以被忽略，它们对于函数不应该造成影响。
+
+ 注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换。
+
+ 在任何情况下，若函数不能进行有效的转换时，请返回 0。
+
+ 说明：
+
+ 假设我们的环境只能存储 32 位大小的有符号整数，那么其数值范围为 [−231,  231 − 1]。如果数值超过这个范围，请返回  INT_MAX (231 − 1) 或 INT_MIN (−231) 。
+
+ 示例 1:
+
+ 输入: "42"
+ 输出: 42
+ 示例 2:
+
+ 输入: "   -42"
+ 输出: -42
+ 解释: 第一个非空白字符为 '-', 它是一个负号。
+      我们尽可能将负号与后面所有连续出现的数字组合起来，最后得到 -42 。
+ 示例 3:
+
+ 输入: "4193 with words"
+ 输出: 4193
+ 解释: 转换截止于数字 '3' ，因为它的下一个字符不为数字。
+ 示例 4:
+
+ 输入: "words and 987"
+ 输出: 0
+ 解释: 第一个非空字符是 'w', 但它不是数字或正、负号。
+ 因此无法执行有效的转换。
+ 示例 5:
+
+ 输入: "-91283472332"
+ 输出: -2147483648
+ 解释: 数字 "-91283472332" 超过 32 位有符号整数范围。
+      因此返回 INT_MIN (−231) 。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/ba-zi-fu-chuan-zhuan-huan-cheng-zheng-shu-lcof
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class ba_zi_fu_chuan_zhuan_huan_cheng_zheng_shu_lcof {
     public static void main(String[] args) {
-        //https://leetcode-cn.com/problems/ba-zi-fu-chuan-zhuan-huan-cheng-zheng-shu-lcof/solution/mian-shi-ti-67-ba-zi-fu-chuan-zhuan-huan-cheng-z-4/
         System.out.println(Integer.MAX_VALUE);
         System.out.println(Integer.MIN_VALUE);
         System.out.println(Integer.toHexString(Integer.MAX_VALUE));
@@ -11,73 +61,36 @@ public class ba_zi_fu_chuan_zhuan_huan_cheng_zheng_shu_lcof {
         System.out.println(Integer.toBinaryString(Integer.MIN_VALUE));
         System.out.println(Integer.toBinaryString(-1));
         System.out.println(Long.MAX_VALUE);
-        //"-91283472332" "20000000000000000000"
+        System.out.println("---------------------");
+        System.out.println(new Solution().strToInt("2147483647"));
+        System.out.println(new Solution().strToInt("-2147483648"));
+        System.out.println(new Solution().strToInt("-2147483649"));
         System.out.println(new Solution().strToInt("-91283472332"));
+        System.out.println(new Solution().strToInt("20000000000000000000"));
+        System.out.println(new Solution().strToInt(" +-100"));
+        System.out.println(new Solution().strToInt(" 4399 word"));
     }
 
     static class Solution {
         public int strToInt(String str) {
-            //init:0 space:1 symbol:2 number:3 end:4
-            //0 -> 1,2,3
-            //1 -> 2,3
-            //2 -> 3
-            //3 -> 4
-            if (str == null) return 0;
-            str = str.trim();
-            Boolean greaterThanZero = null;
-            boolean discoverNumber = false;
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < str.length(); i++) {
-                char c = str.charAt(i);
-                boolean isSpace = c == ' ';
-                boolean isNumber = c >= '0' && c <= '9';
-                boolean isWord = !isSpace && !isNumber;
-                if (discoverNumber) {
-                    if (!isNumber) {
-                        break;
-                    }
-                    if (sb.length() > 0 || c != '0') {
-                        sb.append(c);
-                    }
-                } else {
-                    if (greaterThanZero != null && !isNumber) {
-                        break;
-                    }
-                    if (isNumber) {
-                        discoverNumber = true;
-                        i--;
-                    } else if (c == '+') {
-                        greaterThanZero = true;
-                    } else if (c == '-') {
-                        greaterThanZero = false;
-                    } else if (isWord) {
-                        break;
-                    }
-                }
+            if (str == null || (str = str.trim()).length() == 0) return 0;
+            int sign = 1;
+            int i = 0;
+            if (str.charAt(0) == '-') {
+                sign = -1;
+                i = 1;
             }
-            if (sb.length() > 0) {
-                if (greaterThanZero == null) greaterThanZero = true;
-                if (greaterThanZero && sb.length() > 10) {
-                    return Integer.MAX_VALUE;
+            if (str.charAt(0) == '+') i = 1;
+            int res = 0;
+            for (; i < str.length(); i++) {
+                if ((str.charAt(i) < '0' || str.charAt(i) > '9')) break;
+                int num = str.charAt(i) - '0';
+                if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && num > 7)) {
+                    return sign > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
                 }
-                if (!greaterThanZero && sb.length() > 10) {
-                    return Integer.MIN_VALUE;
-                }
-                long sum = 0;
-                for (int i = 0; i < sb.length(); i++) {
-                    int base = sb.charAt(i) - '0';
-                    long times = (long) Math.pow(10, sb.length() - i - 1);
-                    sum += (base * times);
-                }
-                if (greaterThanZero && sum > (long)Integer.MAX_VALUE) {
-                    return Integer.MAX_VALUE;
-                }
-                if (!greaterThanZero && -sum < (long)Integer.MIN_VALUE) {
-                    return Integer.MIN_VALUE;
-                }
-                return greaterThanZero ? (int)sum : (int)-sum;
+                res = res * 10 + num;
             }
-            return 0;
+            return sign * res;
         }
     }
 }
