0dec7136e04db1e395d2bf91ff05bc31ee4b3e96
typ0520 <php12345@163.com>
2021-03-01 15:23:03 +0800

    reverse integer

--- src/com/example/ListNode.java
+++ src/com/example/ListNode.java
@@ -5,4 +5,9 @@ public class ListNode {
     ListNode next;
 
     ListNode(int x) { val = x; }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
