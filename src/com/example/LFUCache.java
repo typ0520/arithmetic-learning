acf0daa5098ef7dd10c5d01fb95bb2a853b4a061
typ0520 <php12345@163.com>
2021-04-15 23:31:05 +0800

    x

--- src/com/example/LFUCache.java
+++ src/com/example/LFUCache.java
@@ -6,21 +6,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-class LRUCache {
+class LFUCache {
     public static void main(String[] args) {
         List<String> result = Arrays.stream("null,null,null,null,null,null,-1,null,19,17,null,-1,null,null,null,-1,null,-1,5,-1,12,null,null,3,5,5,null,null,1,null,-1,null,30,5,30,null,null,null,-1,null,-1,24,null,null,18,null,null,null,null,-1,null,null,18,null,null,-1,null,null,null,null,null,18,null,null,-1,null,4,29,30,null,12,-1,null,null,null,null,29,null,null,null,null,17,22,18,null,null,null,-1,null,null,null,20,null,null,null,-1,18,18,null,null,null,null,20,null,null,null,null,null,null,null".split(","))
                 .collect(Collectors.toList());
+        String[] options = new String[]{"LFUCache","put","put","get","put","get","get","put","get","get","get"};
+        int[][] params = new int[][]{{2},{1,1},{2,2},{1},{3,3},{2},{3},{4,4},{1},{3},{4}};
 
-        String[] options = new String[]{"LRUCache","put","put","put","put","put","get","put","get","get","put","get","put","put","put","get","put","get","get","get","get","put","put","get","get","get","put","put","get","put","get","put","get","get","get","put","put","put","get","put","get","get","put","put","get","put","put","put","put","get","put","put","get","put","put","get","put","put","put","put","put","get","put","put","get","put","get","get","get","put","get","get","put","put","put","put","get","put","put","put","put","get","get","get","put","put","put","get","put","put","put","get","put","put","put","get","get","get","put","put","put","put","get","put","put","put","put","put","put","put"};
-        int[][] params = new int[][]{{10},{10,13},{3,17},{6,11},{10,5},{9,10},{13},{2,19},{2},{3},{5,25},{8},{9,22},{5,5},{1,30},{11},{9,12},{7},{5},{8},{9},{4,30},{9,3},{9},{10},{10},{6,14},{3,1},{3},{10,11},{8},{2,14},{1},{5},{4},{11,4},{12,24},{5,18},{13},{7,23},{8},{12},{3,27},{2,12},{5},{2,9},{13,4},{8,18},{1,7},{6},{9,29},{8,21},{5},{6,30},{1,12},{10},{4,15},{7,22},{11,26},{8,17},{9,29},{5},{3,4},{11,30},{12},{4,29},{3},{9},{6},{3,4},{1},{10},{3,29},{10,28},{1,20},{11,13},{3},{3,12},{3,8},{10,9},{3,26},{8},{7},{5},{13,17},{2,27},{11,15},{12},{9,19},{2,15},{3,16},{1},{12,17},{9,1},{6,19},{4},{5},{5},{8,1},{11,7},{5,2},{9,28},{1},{2,2},{7,4},{4,22},{7,24},{9,26},{13,28},{11,26}};
-
-        LRUCache cache = new LRUCache(params[0][0]);
+        LFUCache cache = new LFUCache(params[0][0]);
         for (int i = 1; i<options.length; i++) {
             if ("put".equals(options[i])) {
                 cache.put(params[i][0], params[i][1]);
             } else if ("get".equals(options[i])) {
                 int res = cache.get(params[i][0]);
-                if (result.get(i) != null && res != Integer.parseInt(result.get(i))) {
+                if (!"null".equals(result.get(i)) && res != Integer.parseInt(result.get(i))) {
                     System.out.println(i + ": " + cache.get(params[i][0]));
                 }
             }
@@ -35,7 +34,7 @@ class LRUCache {
 
     Map<Integer, Node> cache = new HashMap<>();
 
-    public LRUCache(int capacity) {
+    public LFUCache(int capacity) {
         this.capacity = capacity;
         head = new Node();
         tail = new Node();
@@ -46,7 +45,7 @@ class LRUCache {
     public int get(int key) {
         Node node = cache.get(key);
         if (node == null) return -1;
-        moveToHead(node);
+        incrumentNode(node);
         return node.value;
     }
 
@@ -54,23 +53,44 @@ class LRUCache {
         Node node = cache.get(key);
         if (node != null) {
             node.value = value;
-            moveToHead(node);
+            incrumentNode(node);
             return;
+        }
+        if (size > capacity) {
+            node = removeOne();
+            cache.remove(node.key);
+            size--;
         }
         node = new Node(key, value);
         cache.put(key, node);
         addToHead(node);
+        incrumentNode(node);
         size++;
-        if (size > capacity) {
-            node = removeTail();
-            cache.remove(node.key);
-            size--;
-        }
     }
 
-    private void moveToHead(Node node) {
+    public void incrumentNode(Node node) {
+        node.count += 1;
+        Node cur = node.next;
+        while (cur != tail && cur.count <= node.count) {
+            cur = node.next;
+        }
         removeNode(node);
-        addToHead(node);
+        Node prev = cur.prev;
+        prev.next = node;
+        node.next = cur;
+        cur.prev = node;
+        node.prev = prev;
+    }
+
+    public Node removeOne() {
+        Node cur = head.next;
+        int count = cur.count;
+        while (cur != tail && cur.count == count) {
+            cur = cur.next;
+        }
+        Node res = cur.prev;
+        removeNode(cur.prev);
+        return res;
     }
 
     private void removeNode(Node node) {
@@ -85,15 +105,10 @@ class LRUCache {
         head.next = node;
     }
 
-    private Node removeTail() {
-        Node node = tail.prev;
-        removeNode(node);
-        return node;
-    }
-
     public static class Node {
         int key;
         int value;
+        int count;
         Node prev;
         Node next;
 
@@ -107,8 +122,8 @@ class LRUCache {
 }
 
 /**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
